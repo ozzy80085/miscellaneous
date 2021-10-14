9fae7f5833c606a00d640192b533d023e58a6eef
ozzy <65918625+ozzy80085@users.noreply.github.com>
2021-10-14 10:37:22 -0400

    Create wheresmyinterpreter.py

---
+++ wheresmyinterpreter.py
@@ -0,0 +1,7 @@
+import sys
+
+print(sys.executable)
+
+#this is a program that will tell you the path of your python interpreter 
+#for example if your trying to find your python2 interpreter you run "python wheresmyinterpreter.py"
+#or to find your python3 interpreter you run "python3 wheresmyinterpreter.py"
