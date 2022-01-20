c1a5f34c51d7ae4088fc55fb18294648fd750edd
ozzy <65918625+ozzy80085@users.noreply.github.com>
2022-01-20 15:51:59 -0500

    Update purge.py

--- purge.py
+++ purge.py
@@ -1,10 +1,33 @@
 import os
+import time
 
-e = input("whats the path to your file that you would like to purge: ")
+os.system("clear")
+e = input("Whats the path to your file that you would like to purge: ")
 
-print("Purging... (should take ~10 seconds)")
-os.system(f"dd if=/dev/urandom of={e} & sleep 10 ; kill $!")
-os.system(f"rm {e}")
+#File content purge
+os.system("clear")
+print("Purging... (pass 1)")
+os.system(f"dd if=/dev/zero of={e} & sleep 5; kill $!")
+os.system("clear")
+print("Purging... (pass 2)")
+os.system(f"dd if=/dev/zero of={e} & sleep 5; kill $!")
+os.system("clear")
+print("Purging... (pass 3)")
+os.system(f"dd if=/dev/zero of={e} & sleep 5; kill $!")
+os.system("clear")
+print("Purging... (pass 4)")
+os.system(f"dd if=/dev/zero of={e} & sleep 5; kill $!")
+os.system("clear")
+print("File contents successfully purged")
+
+
+#Filename purge
+print("Purging filename...")
+os.system(f"mv {e} /tmp/0000")
+os.system(f"mv /tmp/0000 /tmp/000")
+os.system(f"mv /tmp/000 /tmp/00") 
+os.system(f"mv /tmp/00 /tmp/0")
+
+#removing the file
+os.system(f"rm /tmp/0")
 print(f"{e} successfully purged")
-
-#due to the dd and rm tool only being available in linux based distros this wont work on windows
