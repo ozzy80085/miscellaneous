a5225338fb5c1989dc90031f780fff1bb766e840
ozzy <65918625+ozzy80085@users.noreply.github.com>
2022-03-01 12:24:13 -0500

    Create telnetmsg.py

---
+++ telnetmsg.py
@@ -0,0 +1,26 @@
+import socket
+
+#the following sends the specified message then close the connection but continue to listen for incomming connections
+#i can think of a reason when this would be useful but you never know
+
+
+HOST = "127.0.0.1"  
+PORT = 23
+
+msg = b"""
+
+Hello World!
+
+"""
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+
+while True:
+    s.listen()
+    conn, addr = s.accept()
+    
+    with conn:
+        print(f"Connected by {addr}")
+        conn.sendall(msg)
+        conn.close()
