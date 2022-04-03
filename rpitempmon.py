d53d71520742d4d08cf76fe7a0917bb79c65a301
ozzy <65918625+ozzyisgreat@users.noreply.github.com>
2022-04-03 10:51:53 -0400

    Delete rpitempmon.py

--- rpitempmon.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import CPUTemperature
-
-cpu = CPUTemperature()
-cel = cpu.temperature
-fah = cpu.temperature * 9/5 + 32
-print("in fahrenheit your cpu temperature is", fah)
-print("in celsius your cpu temperature is", cel)
