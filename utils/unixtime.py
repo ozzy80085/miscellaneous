0ef341c81758dc38b1eb8e9483bb27879db7d7c7
ozzy <65918625+ozzyisgreat@users.noreply.github.com>
2022-05-08 10:04:51 -0400

    Update unixtime.py

--- utils/unixtime.py
+++ utils/unixtime.py
@@ -3,6 +3,8 @@
 import time
 import os
 
+#one liner install
+#sudo curl "https://raw.githubusercontent.com/ozzyisgreat/miscellaneous/main/utils/unixtime.py" -o /bin/unixtime; sudo chmod +x /bin/unixtime; echo Done! type "unixtime" to run
 
 while True:
         t = str(time.time())
