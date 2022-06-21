4d773734ee40a2b34d6814b1836744c282eaea44
ozzy <65918625+ozzyisgreat@users.noreply.github.com>
2022-06-21 15:01:38 -0400

    Create btcpriceV2.py

---
+++ btcpriceV2.py
@@ -0,0 +1,16 @@
+import requests
+import bs4
+
+r = requests.get("https://www.coindesk.com/price/bitcoin/")
+
+soup = bs4.BeautifulSoup(r.text, features="lxml")
+
+price = soup.find("span", {"class": "typography__StyledTypography-owin6q-0 ktzuAh"})
+price = "".join(price.strings)
+
+hr = soup.find("h6", {"class": "typography__StyledTypography-owin6q-0 bclXpS"})
+hr = "".join(hr.strings)
+
+print(f"""Current BTC Price: {price}
+The price has changed {hr} in the last 24 hours
+""")
